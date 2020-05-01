9e8bdeb757ac051957cc652be0269c5ecef1d864
Li-Wei Hung <li-wei_hung@nps.gov>
2020-05-01 17:18:17 -0600

    estimated the signal to noise ratio when doing the photometry

--- cropimages.py
+++ cropimages.py
@@ -8,7 +8,7 @@
 #This script crops the center of the images and copy the header with 
 #modifications on the image size entries.
 #
-#Input: Test_Images/Coyote_Ridge/30sec.4_2x2_dark_subtracts.fit
+#Input: Test_Images/20200427/30_sec_V_light_2x2.fit
 #
 #Output:
 #   (1) A set of cropped images in the Test_Images/Coyote_Ridge_Crop folder
@@ -25,15 +25,15 @@ from matplotlib import pyplot as plt
 #-----------------------------------------------------------------------------#
 
 # Load the FITS hdulist using astropy.io.fits
-filename = "Test_Images/Coyote_Ridge/30sec.4_2x2_dark_subtracts.fit"
+filename = "Test_Images/20200427/30_sec_V_light_2x2.fit"
 hdulist = fits.open(filename)
 hdr = hdulist[0].header
 img = hdulist[0].data
 (y,x) = img.shape
 
-outfolder = "Test_Images/Coyote_Ridge_Crop/30sec.4_2x2_dark_subtracts"
+outfolder = "Test_Images/20200427_Crop/30_sec_V_light_2x2"
 
-for s in n.arange(200,1500,100):
+for s in n.arange(1000,1500,100):
 	crop = img[int(y/2-s/2):int(y/2+s/2),int(x/2-s/2):int(x/2+s/2)]
 	hdr['NAXIS1'] = s
 	hdr['NAXIS2'] = s
--- photometry.py
+++ photometry.py
@@ -10,12 +10,12 @@
 #
 #Input: 
 #   (1) 'hipparcos_bright_standards.txt' -- Standard star catalog
-#	(2) 'Test_Images/Coyote_Ridge_Astrometry/corr.fits' -- matched star list
-#	(3) 'Test_Images/Coyote_Ridge_Astrometry/wcs.fits' -- wcs coordinates
-#	(4) 'Test_Images/Coyote_Ridge_Astrometry/30sec.4_2x2_dark_subtracts_crop1400.fit'
+#	(2) 'Test_Images/20200427_Astrometry/corr.fits' -- matched star list
+#	(3) 'Test_Images/20200427_Astrometry/wcs.fits' -- wcs coordinates
+#	(4) 'Test_Images/20200427_Astrometry/30_sec_V_light_2x2_crop1300.fit'
 #
-#	(2) 'Test_Images/Coyote_Ridge_Astrometry/new-image.fits'
-#	(3) 'Test_Images/Coyote_Ridge_Astrometry/rdls.fits'	
+#	(2) 'Test_Images/20200427_Astrometry/new-image.fits'
+#	(3) 'Test_Images/20200427_Astrometry/rdls.fits'	
 #	(5) 
 #
 #Output:
@@ -51,8 +51,8 @@ def Gaussian_2d(xy, x0, y0, sigma, V):
 	return g.ravel()
 	
 #-----------------------------------------------------------------------------#
-#Merge the standard stars and astrometry star files
-
+#			  Merge the standard stars and astrometry star files			  #
+#-----------------------------------------------------------------------------#
 #Standard stars
 hips = n.loadtxt('hipparcos_bright_standards.txt')
 H = pd.DataFrame(hips, columns=['HIP','Vmag','RA','DE','B-V','V-I'])
@@ -60,7 +60,7 @@ H = H.round({'RA':2,'DE':2})
 H.set_index(['RA','DE'], inplace=True)
 
 #Astrometry file contains correspondences between image and reference stars 
-hdu = fits.open('Test_Images/Coyote_Ridge_Astrometry/corr.fits')
+hdu = fits.open('Test_Images/20200427_Astrometry/corr.fits')
 C = pd.DataFrame.from_records(hdu[1].data).astype('float64').round(2)
 C.rename(columns={'index_ra':'RA','index_dec':'DE'},inplace=True)
 C.rename(columns={'FLUX':'Flux','BACKGROUND':'Background'},inplace=True)
@@ -75,11 +75,12 @@ T.reset_index(inplace=True)
 T.field_x -= 1
 T.field_y -= 1
 
-#-----------------------------------------------------------------------------#
-#Zenith angle and airmass 
 
+#-----------------------------------------------------------------------------#
+#							Zenith angle and airmass 						  #
+#-----------------------------------------------------------------------------#
 #Read in the WCS coordinates
-hdu2 = fits.open('Test_Images/Coyote_Ridge_Astrometry/wcs.fits', fix=False)
+hdu2 = fits.open('Test_Images/20200427_Astrometry/wcs.fits', fix=False)
 w = WCS(hdu2[0].header)
 
 #Convert the central pixel to world coordinates w/ 0-based coordinates
@@ -94,12 +95,13 @@ T['ZA'] = n.round(90-AL, 2)  #star zenith angle
 #Airmass: compute and add to the dataframe
 T['Airmass'] = 1/n.cos(n.deg2rad(T['ZA'])) #airmass
 
-#-----------------------------------------------------------------------------#
-#Absolute photometry with Gaussian PSF: calculate flux and background
 
+#-----------------------------------------------------------------------------#
+#	 Absolute photometry with Gaussian PSF: calculate flux and background	  #
+#-----------------------------------------------------------------------------#
 #open the original image
-imgname = '30sec.4_2x2_dark_subtracts_crop1400.fit'
-hdu3 = fits.open('Test_Images/Coyote_Ridge_Astrometry/'+imgname, fix=False)
+imgname = '30_sec_V_light_2x2_crop1300.fit'
+hdu3 = fits.open('Test_Images/20200427_Astrometry/'+imgname, fix=False)
 data = hdu3[0].data
 exptime = hdu3[0].header['EXPTIME']
 
@@ -135,36 +137,45 @@ for i in range(len(T)):
 		
 	#set the acceptance threshold to record the measurement
 	delta_position = n.sum(((popt-p0)[0:2]**2))**0.5   #position diff [pix]
-	sigma = abs(popt[2])          				       #sigma of the gaussian
-	signal = popt[3]/ bg       						   #brightness/background
-
-	if delta_position<1 and sigma<2 and signal>10:
+	sigma = abs(popt[2])          				       #sigma of the 2d gaussian
+	npix = n.pi*(3*sigma)**2 	   #numberof pixels in the approximated aperture
+	source_noise = popt[3]*exptime #noise from the source
+	sky_noise = npix*bg*exptime	   #noise from the sky background
+	dark_and_read_noise = 0		   #dark and read noise http://www2.lowell.edu/rsch/LMI/ETCMethod.pdf
+								   #FIXME http://www.astro.wisc.edu/~sheinis/ast500/AY500_lect5.ppt.pdf	
+	totalnoise = n.sqrt(source_noise+sky_noise+dark_and_read_noise)
+	signal_to_noise = popt[3]*exptime/totalnoise       						
+	
+	if delta_position<1 and sigma<2 and signal_to_noise>60:		#FIXME Adjust the SN ratio once the noise reduction is implimented!
 		T.loc[i,['field_x','field_y']] = popt[0], popt[1]
 		T.loc[i,['Flux','Background']] = popt[3], bg   #[counts/sec]
-		T.loc[i,'fit'] = True 
+		T.loc[i,'fit'] = True
+		T.loc[i,'SN'] = signal_to_noise
+		T.loc[i,'Aperture'] = npix #background aperture radius [pix]		
 
 #delete the rows with bad photometry measurements
 print('%s stars are used in the photometric calibration.' % sum(T.fit==True))
 print('%s stars rejected due to bad photometry.' % sum(T.fit==False))
 T.drop(T[T.fit==False].index, inplace=True)
 T.drop(columns='fit', inplace=True)
-T = T.round({'field_x':2,'field_y':2,'Flux':2,'Background':2})
+T = T.round({'field_x':2,'field_y':2,'Flux':2,'Background':2,'SN':2,'Sigma':2})
 
 
-#----------------------------
-#Zeropoint and extinction
-
-T['m'] = -2.5*n.log10(T.Flux)               #v_mag, apparent
+#-----------------------------------------------------------------------------#
+#						Zeropoint and extinction fitting					  #
+#-----------------------------------------------------------------------------#
+#apparent magnitude of the background-subtraced stellar flux [counts/sec]
+T['m'] = -2.5*n.log10(T.Flux)
 T.dropna(inplace=True)
 
+#fitting for zeropoint and extinction
 param, cov = n.polyfit(T.Airmass, T.Vmag-T.m, 1, cov=True)
 c, z = param                         #bestfit coefficient and zeropoint
 c_err, z_err = n.sqrt(cov.diagonal())#uncertainties
 
-
+#plotting
 fig = plt.figure('zeropoint')
 plt.plot(T.Airmass, T.Vmag-T.m, 'o', label='Hipparcos standard stars')
-
 a = n.arange(n.ceil(max(T.Airmass))+1)
 plt.plot(a,c*a+z,'-',lw=2,label='Best fit: %.2fx+%.3f' %(c,z))
 plt.errorbar(0,z,z_err,fmt='o',label='zeropoint: %.3f+-%.3f'%(z,z_err))
@@ -177,9 +188,11 @@ plt.title('Zeropoint and Extinction Coefficient',fontsize=14)
 #plt.close('zeropoint')
 plt.show(block=False)
 
+
+#-----------------------------------------------------------------------------#
 #import warnings
 #warnings.filterwarnings("ignore")
-#hdu3 = fits.open('Test_Images/Coyote_Ridge_Astrometry/wcs.fits')
+#hdu3 = fits.open('Test_Images/20200427_Astrometry/wcs.fits')
 
 
 

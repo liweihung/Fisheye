01780490b0669aa451aa587ca9d28df8d5411aed
Li-Wei Hung <li-wei_hung@nps.gov>
2020-10-21 17:47:44 -0600

    added upper hammer class

--- projection.py
+++ projection.py
@@ -17,7 +17,7 @@
 #History:
 #	Li-Wei Hung -- Created 
 #
-#-----------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
 import matplotlib.projections as mprojections
 import numpy as n
 
@@ -32,72 +32,46 @@ import matplotlib.spines as mspines
 # Local Source
 import colormaps
 import filepath
+import upper_hammer
 
 #------------------------------------------------------------------------------#
-#					  Create Upper Hammer Projection Class				   	   #
-#------------------------------------------------------------------------------#
-class UpperHammerAxes(HammerAxes):
-	name = 'upper_hammer'
-	def cla(self):
-		HammerAxes.cla(self)
-		Axes.set_xlim(self, -n.pi, n.pi)
-		Axes.set_ylim(self, 0, n.pi / 2.0)
-		self.xaxis.set_ticks_position('top')
-		self.tick_params(axis='x', length=0)
-		self.tick_params(colors='darkgray')
-		self.grid(color='gray', linestyle='dotted', linewidth=.5)
-	
-	def _gen_axes_patch(self):
-		return Wedge((0.5, 0.5), 0.5, 0, 180)
-	
-	def _gen_axes_spines(self):
-		pass
-		path = Wedge((0, 0), 1.0, 0, 180).get_path()
-		spine = mspines.Spine(self, 'circle', path)
-		spine.set_color('gray')
-		spine.set_patch_circle((0.5, 0.5), 0.5)
-		return {'wedge':spine}
-		
-mprojections.register_projection(UpperHammerAxes)
-
 #------------------------------------------------------------------------------#
 #						  Define Plot settings							   	   #
 #------------------------------------------------------------------------------#
-plt.close('all')
-plt.style.use('dark_background')
-plt.rcParams['image.cmap'] = 'NPS_mag'
-plt.cm.get_cmap().set_bad(color='black')
-
-def fisheye_plot(theta, r, img):
+def fisheye_plot(img):
 	"""
 	Plots the input image in a fisheye view. Saves the output as a png image. 
 	"""
 	fig = plt.figure() 
 	ax = plt.subplot(111, projection='polar')
-	ax.pcolormesh(theta,r,img,vmin=14,vmax=24)
+	ax.pcolormesh(theta,r_deg,img,vmin=14,vmax=24)
 	ax.set_rlim(0,90)
 	ax.set_yticklabels([])
 	ax.tick_params(colors='darkgray')
 	ax.set_theta_zero_location('N') 
 	ax.grid(True, color='gray', linestyle='dotted', linewidth=.5)
-	#plt.savefig('.png', dpi=200)
+	plt.savefig(f[:-4]+'_fisheye.png', dpi=300)
 	
 	
-def hammer_plot(theta,r,img):
+def hammer_plot(img):
 	"""
 	Plots the input image in Hammer equal area projection. Saves the output as 
 	a png image.
 	"""
-	plt.figure(figsize=(11,3.8))
+	plt.figure(figsize=(15,5.2))
 	ax = plt.subplot(111, projection="upper_hammer")
-	ax.pcolormesh(theta,r,img,vmin=14,vmax=24)
+	ax.pcolormesh(theta_sorted,r_sorted,img,vmin=14,vmax=24)
 	ax.grid(True)
 	#plt.title("Hammer")
 	plt.tight_layout(rect=(0.03,-0.6,0.98,0.97))
-#------------------------------------------------------------------------------#
-#------------------------------------------------------------------------------#
-#			  Input files												   	   #
-#------------------------------------------------------------------------------#
+	plt.savefig(f[:-4]+'_hammer.png')
+
+
+#General plot settings
+plt.close('all')
+plt.style.use('dark_background')
+plt.rcParams['image.cmap'] = 'NPS_mag'
+plt.cm.get_cmap().set_bad(color='black')	
 
 #Mask - read in the fisheye mask center coordinates and radius
 mask = fits.open(filepath.mask,uint=False)[0].header
@@ -108,25 +82,27 @@ X, Y = n.meshgrid(n.arange(-r0,r0),n.arange(-r0,r0))
 r = n.sqrt(X**2+Y**2) / r0
 theta = n.arctan2(Y,X)
 
-for f in glob(filepath.data_cal+'*light*'):
-	img = fits.open(f,uint=False)[0].data[yc-r0:yc+r0,xc-r0:xc+r0] 
-
-#-----------------------------------------------------------------------------#
-#							Plot the fitting results						  #
-#-----------------------------------------------------------------------------#
 #Fisheye takes r in degree
 r_deg = 90*r
-fisheye_plot(theta, r_deg, img)
 
 #Hammer plot requires the values to be sorted
 r_str = n.pi/2 - r * n.pi/2
 inds = n.argsort(theta[:,0])
 theta_sorted = theta[inds,:] 
 r_sorted = r_str[inds,:]
-img_sorted = img[inds,:]
-hammer_plot(theta_sorted, r_sorted, img_sorted)
 
-plt.show(block=False)
+#------------------------------------------------------------------------------#
+#				Plot the image in fisheye and Hammer projections			   #
+#------------------------------------------------------------------------------#
+for f in glob(filepath.data_cal+'*light*.fit'):
+	print(f)
+	img = fits.open(f,uint=False)[0].data[yc-r0:yc+r0,xc-r0:xc+r0] 
+	#fisheye_plot(img)
+	hammer_plot(img[inds,:])
+
+#plt.show(block=False)
+#plt.savefig(f+'_hammer4.jpg', dpi=300)
+
 '''
 #----------------old code below------------------------------------------------#
 
---
+++ upper_hammer.py
@@ -0,0 +1,104 @@
+#-----------------------------------------------------------------------------#
+#upper_hammer.py
+#
+#NPS Night Skies Program
+#
+#Last updated: 2020/10/21
+#
+#This script 
+#
+#Input: 
+#   (1) 
+#
+#Output:
+#   (1) 
+#
+#History:
+#	Li-Wei Hung -- Created 
+#
+#-----------------------------------------------------------------------------#
+
+import matplotlib.projections as mprojections
+import matplotlib.spines as mspines
+import numpy as n
+
+from matplotlib.axes import Axes
+from matplotlib.patches import Wedge
+from matplotlib.projections.geo import HammerAxes
+from matplotlib.ticker import FixedLocator
+from matplotlib.transforms import Affine2D
+
+
+class UpperHammerAxes(HammerAxes):
+	name = 'upper_hammer'
+	def cla(self):
+		HammerAxes.cla(self)
+		Axes.set_xlim(self, -n.pi, 2*n.pi)
+		Axes.set_ylim(self, 0, n.pi / 2.0)
+		self.xaxis.set_ticks_position('top')
+		self.tick_params(axis='x', length=0)
+		self.tick_params(colors='grey', labelsize=11)
+		self.grid(color='gray', linestyle='dotted', linewidth=.5)
+		self.set_yticks(n.linspace(0, n.pi/2, 7)) #include latitude 90 label
+		self.set_xticklabels(['N','30°','60°','E','120°','150°','S','210°','240°','W','300°','330°','N'])
+
+	
+	def _gen_axes_patch(self):
+		return Wedge((0.5, 0.5), 0.5, 0, 180)
+	
+	def _gen_axes_spines(self):
+		pass
+		path = Wedge((0, 0), 1.0, 0, 180).get_path()
+		spine = mspines.Spine(self, 'circle', path)
+		spine.set_color('gray')
+		spine.set_patch_circle((0.5, 0.5), 0.5)
+		return {'wedge':spine}
+
+	def _set_lim_and_transforms(self):
+		super()._set_lim_and_transforms()
+		
+		self._xaxis_text2_transform = \
+			Affine2D().scale(1.0, 0.0) + \
+			self.transData + \
+			Affine2D().translate(0.0, -12.0)
+		
+		yaxis_stretch = Affine2D().scale(n.pi*2, 1.02).translate(-n.pi, 0)
+		yaxis_space = Affine2D().scale(1.0, 1.05)
+		
+		yaxis_text_base = \
+			yaxis_stretch + \
+			self.transProjection + \
+			(yaxis_space +
+			self.transAffine +
+			self.transAxes)
+		
+		self._yaxis_text1_transform = \
+			yaxis_text_base + \
+			Affine2D().translate(-6, -4)
+		
+	def get_yaxis_text1_transform(self, pad):
+		return self._yaxis_text1_transform, 'center', 'right'
+		
+	def set_longitude_grid(self, degrees):
+		"""
+		Set the number of degrees between each longitude grid.
+	
+		This is an example method that is specific to this projection
+		class -- it provides a more convenient interface to set the
+		ticking than set_xticks would.
+		"""
+		grid = n.linspace(-180, 180, 360/degrees+1)
+		self.xaxis.set_major_locator(FixedLocator(n.deg2rad(grid)))
+		self.xaxis.set_major_formatter(self.ThetaFormatter(degrees))
+	
+		
+mprojections.register_projection(UpperHammerAxes)
+
+if __name__ == '__main__':
+	import matplotlib.pyplot as plt
+	#plt.close('all')
+	fig = plt.figure(figsize=(15,5.2))
+	ax = fig.add_subplot(111, projection='upper_hammer')
+	ax.grid(True)
+	plt.tight_layout(rect=(0.03,-0.6,0.98,0.97))
+	plt.show()

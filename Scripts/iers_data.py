90f5d825fc85f3957d653a4b99e66133e0435031
Zach Vanderbosch <zvanderbosch@nps.gov>
2024-12-04 16:57:23 -0700

    Add check on local IERS table ages

--- Scripts/iers_data.py
+++ Scripts/iers_data.py
@@ -27,6 +27,9 @@
 #    Zach Vanderbosch -- Created on 11/25/2024
 #-----------------------------------------------------------#
 
+import os
+import time
+import stat
 import pathlib
 import requests
 import astropy_iers_data as aid
@@ -37,9 +40,14 @@ import astropy_iers_data as aid
 #       https://github.com/astropy/astropy-iers-data        #
 #-----------------------------------------------------------#
 
+# Set maximum IERS Table age (in seconds).
+# If an IERS Table already exists locally and is less than 
+# this age, it will not be re-downloaded, saving some time.
+MAX_TABLE_AGE = 86400.0  # 86400 sec = 1 day
+
 # Path where tables are saved (astropy-iers-data package location).
-# This is where all astropy routines will look for them when the
-# astropy.utils.iers.conf parameter auto_download = False.
+# This is where all astropy routines will look for the tables when
+# the astropy.utils.iers.conf parameter auto_download = False.
 DATA = pathlib.Path(aid.__file__).resolve().parent / "data"
 
 # IERS-A default file name, URL, and ReadMe with content description
@@ -81,23 +89,55 @@ def download_file(url, filename):
     return status
 
 
+# Function to Check File Ages if they already exist
+def check_file_age(filename):
+
+    if os.path.isfile(filename):
+        time_since_mod = time.time() - os.stat(filename)[stat.ST_MTIME]
+        return time_since_mod
+    else:
+        return np.nan
+
+
+
 # Execute file downloads
 def main():
 
     print('Updating IERS tables...')
 
     # Try downloading IERS-A file
-    status = download_file(IERS_A_URL, IERS_A_FILE)
-    if status != 200:
-        status = download_file(IERS_A_URL_MIRROR, IERS_A_FILE)
+    file_age = check_file_age(IERS_A_FILE)
+    if file_age > MAX_TABLE_AGE:
+        status = download_file(IERS_A_URL, IERS_A_FILE)
+        if status != 200:
+            status = download_file(IERS_A_URL_MIRROR, IERS_A_FILE)
+    elif file_age < MAX_TABLE_AGE:
+        print('  Existing %s table found with age less than %.1d day' %(
+            IERS_A_FILE.split("\\")[-1], 
+            MAX_TABLE_AGE/86400
+        ))
 
     # Try downloading IERA-B file
-    status = download_file(IERS_B_URL, IERS_B_FILE)
+    file_age = check_file_age(IERS_B_FILE)
+    if file_age > MAX_TABLE_AGE:
+        status = download_file(IERS_B_URL, IERS_B_FILE)
+    elif file_age < MAX_TABLE_AGE:
+        print('  Existing %s table found with age less than %.1d day' %(
+            IERS_B_FILE.split("\\")[-1], 
+            MAX_TABLE_AGE/86400
+        ))
 
     # Try downloading Leap Seconds file
-    status = download_file(IERS_LEAP_SECOND_URL, IERS_LEAP_SECOND_FILE)
-    if status != 200:
-        status = download_file(IERS_LEAP_SECOND_URL_MIRROR, IERS_LEAP_SECOND_FILE)
+    file_age = check_file_age(IERS_LEAP_SECOND_FILE)
+    if file_age > MAX_TABLE_AGE:
+        status = download_file(IERS_LEAP_SECOND_URL, IERS_LEAP_SECOND_FILE)
+        if status != 200:
+            status = download_file(IERS_LEAP_SECOND_URL_MIRROR, IERS_LEAP_SECOND_FILE)
+    elif file_age < MAX_TABLE_AGE:
+        print('  Existing %s table found with age less than %.1d day' %(
+            IERS_LEAP_SECOND_FILE.split("\\")[-1], 
+            MAX_TABLE_AGE/86400
+        ))
 
 
 # Allow script to be run directly
